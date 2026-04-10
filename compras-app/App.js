2dda97c5e70fbac2d39c56e023aedcddcfbfdb34
luizcastilho2ds <luizota2009@gmail.com>
2026-04-10 12:08:56 -0300

    Add files via upload

--- compras-app/App.js
+++ compras-app/App.js
@@ -1,35 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-// You can import supported modules from npm
-import { Card } from 'react-native-paper';
-
-// or any files within the Snack
-import AssetExample from './components/AssetExample';
+import React, { useState, useEffect } from "react";
+import { View, Image, Text } from "react-native";
+import Login from "./src/telas/Login";
+import Cadastro from "./src/telas/Cadastro";
+import Cesta from "./src/telas/Cesta";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [telaAtual, setTelaAtual] = useState("Login"); // controle de telas
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const itens = [
+    { id: "1", nome: "Alface", imagem: "https://img.cdndsgni.com/preview/13206058.jpg"},
+    { id: "2", nome: "Tomate", imagem: "https://img.cdndsgni.com/preview/10002716.jpg" },
+    { id: "3", nome: "Cenoura", imagem: "https://img.cdndsgni.com/preview/12485137.jpg" },
+    { id: "4", nome: "Batata", imagem: "https://img.cdndsgni.com/preview/10600699.jpg" },
+  ];
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#fff" }}>
+        <Image source={require("./assets/splash.png")} style={{ width: 200, height: 200, resizeMode: "contain" }} />
+        <Text style={{ marginTop: 20, fontSize: 18, fontWeight: "bold" }}>Compras App</Text>
+      </View>
+    );
+  }
+
+  if (telaAtual === "Login") {
+    return (
+      <Login
+        irParaCatalogo={() => setTelaAtual("Cesta")}
+        irParaCadastro={() => setTelaAtual("Cadastro")}
+      />
+    );
+  }
+
+  if (telaAtual === "Cadastro") {
+    return <Cadastro voltar={() => setTelaAtual("Login")} />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
-    </View>
+    <Cesta
+      nome="Cesta de Verduras"
+      fazenda="Fazenda Boa Terra"
+      preco="R$ 40,00"
+      imagemFazenda="https://images.creativefabrica.com/products/previews/2023/09/21/zflmFfpQ1/2arIMKaNH3p63xC93CBzRYqmF9Z-desktop.jpg"
+      itens={itens}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
