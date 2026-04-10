83993037d3fac2a16089c4dbb35508d269edff58
luizcastilho2ds <luizota2009@gmail.com>
2026-04-10 12:18:41 -0300

    Add files via upload

--- compras-app/App.js
+++ compras-app/App.js
@@ -2,29 +2,26 @@ import React, { useState, useEffect } from "react";
 import { View, Image, Text } from "react-native";
 import Login from "./src/telas/Login";
 import Cadastro from "./src/telas/Cadastro";
-import Cesta from "./src/telas/Cesta";
+import Catalogo from "./src/telas/Catalogo";
+import Carrinho from "./src/telas/Carrinho";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
-  const [telaAtual, setTelaAtual] = useState("Login"); // controle de telas
+  const [telaAtual, setTelaAtual] = useState("Login");
+  const [carrinho, setCarrinho] = useState([]);
 
   useEffect(() => {
     const timer = setTimeout(() => setLoading(false), 2000);
     return () => clearTimeout(timer);
   }, []);
 
-  const itens = [
-    { id: "1", nome: "Alface", imagem: "https://img.cdndsgni.com/preview/13206058.jpg"},
-    { id: "2", nome: "Tomate", imagem: "https://img.cdndsgni.com/preview/10002716.jpg" },
-    { id: "3", nome: "Cenoura", imagem: "https://img.cdndsgni.com/preview/12485137.jpg" },
-    { id: "4", nome: "Batata", imagem: "https://img.cdndsgni.com/preview/10600699.jpg" },
-  ];
+  const adicionarAoCarrinho = (item) => setCarrinho([...carrinho, item]);
 
   if (loading) {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#fff" }}>
-        <Image source={require("./assets/splash.png")} style={{ width: 200, height: 200, resizeMode: "contain" }} />
-        <Text style={{ marginTop: 20, fontSize: 18, fontWeight: "bold" }}>Compras App</Text>
+      <View style={{ flex:1, justifyContent:"center", alignItems:"center", backgroundColor:"#fff" }}>
+        <Image source={require("./assets/splash.png")} style={{ width:200, height:200, resizeMode:"contain" }} />
+        <Text style={{ marginTop:20, fontSize:18, fontWeight:"bold" }}>Compras App</Text>
       </View>
     );
   }
@@ -32,7 +29,7 @@ export default function App() {
   if (telaAtual === "Login") {
     return (
       <Login
-        irParaCatalogo={() => setTelaAtual("Cesta")}
+        irParaCatalogo={() => setTelaAtual("Catalogo")}
         irParaCadastro={() => setTelaAtual("Cadastro")}
       />
     );
@@ -42,13 +39,15 @@ export default function App() {
     return <Cadastro voltar={() => setTelaAtual("Login")} />;
   }
 
-  return (
-    <Cesta
-      nome="Cesta de Verduras"
-      fazenda="Fazenda Boa Terra"
-      preco="R$ 40,00"
-      imagemFazenda="https://images.creativefabrica.com/products/previews/2023/09/21/zflmFfpQ1/2arIMKaNH3p63xC93CBzRYqmF9Z-desktop.jpg"
-      itens={itens}
-    />
-  );
+  if (telaAtual === "Catalogo") {
+    return <Catalogo irParaCarrinho={() => setTelaAtual("Carrinho")} adicionarAoCarrinho={adicionarAoCarrinho} />;
+  }
+
+  if (telaAtual === "Carrinho") {
+    return <Carrinho itens={carrinho} voltarCatalogo={() => setTelaAtual("Catalogo")} />;
+  }
+
+  return null;
 }
+
+
